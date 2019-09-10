35a4e7a41e8730d88a8cf567cae6f89e91a94a72
flakj <jacek.flak@contractors.roche.com>
2019-09-10 15:49:55 +0200

    RestControllers added Servlet logger removed

--- src/main/java/com/example/demo/HelloServlet.java
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -2,22 +2,27 @@ package com.example.demo;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class HelloServlet extends HttpServlet {
+//
+@RestController
+@RequestMapping("/api")
+public class HelloServlet {
 
     private final Logger logger = LoggerFactory.getLogger(HelloServlet.class);
 
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+    @GetMapping("/test")
+    protected String doGet(HttpServletRequest req) throws ServletException, IOException {
         logger.info("Request got");
-        resp.getWriter().write("Text from response");
+        return "Działam :)";
     }
 }

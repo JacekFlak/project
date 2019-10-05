c9b56c35035d8a5ce90fae92b9e470b13c543fe2
Jacek <jacek.flak.kontakt@gmail.com>
2019-10-05 16:48:59 +0200

    properties changed

--- src/main/java/com/jacek/demoprojekt/modal/User.java
+++ src/main/java/com/jacek/demoprojekt/modal/User.java
@@ -21,44 +21,4 @@ public class User {
     public User(String username, String firstname, String lastname, String password) {
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public String getusername() {
-        return this.username;
-    }
-
-    public String getFirstname() {
-        return this.firstname;
-    }
-
-    public String getLastname() {
-        return this.lastname;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
 }

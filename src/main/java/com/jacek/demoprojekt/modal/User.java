145e92e0a0f74a641ec67bd5fd2a926da5a56a9c
Jacek <jacek.flak.kontakt@gmail.com>
2019-10-06 22:39:41 +0200

    changes

--- src/main/java/com/jacek/demoprojekt/modal/User.java
+++ src/main/java/com/jacek/demoprojekt/modal/User.java
@@ -2,9 +2,8 @@ package com.jacek.demoprojekt.modal;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 
 @Entity
 @Table(name = "users")
@@ -12,6 +11,8 @@ import javax.persistence.Table;
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
     private int id;
     private String username;
     private String firstname;

d40a757f7c33e2a10f62349df149579b88b0ab7d
Jacek <jacek.flak.kontakt@gmail.com>
2019-11-04 15:34:18 +0100

    @Secured added

---
+++ src/main/java/com/jacek/projekt/admin/AdminPageController.java
@@ -0,0 +1,18 @@
+package com.jacek.projekt.admin;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class AdminPageController {
+
+    @RequestMapping(value = "/admin", method = RequestMethod.GET)
+    @Secured(value = {"ROLE_ADMIN"})
+    public String openAdminMainPage() {
+        return "admin/admin";
+    }
+
+}
